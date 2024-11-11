05e75d1a2c1ae02a5cb7526ac01a96f8baae2db0
Ayush Srivastav <139707217+Ayush30s@users.noreply.github.com>
2024-11-11 12:59:48 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -50,7 +50,7 @@ const Home = ({ d, setD }) => {
           </a>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center text-center md:text-start items-center mt-12">
+        <div className="flex flex-col md:flex-row justify-center text-center md:text-start items-center? mt-8 md:mt-12">
           <div className="w-[90%] md:w-[60%] flex flex-col justify-center  items-center md:items-start">
             <h1 className="text-[20px] font-bebas">
               {!dark ? "I am," : "I am,"}
