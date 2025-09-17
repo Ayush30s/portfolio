63942e80dcdc45afd894b6c9c94c3428da6ad9d3
Ayush30s <ayushsri8318328947@gmail.com@users.noreply.github.com>
2025-09-17 14:56:24 +0530

    onr

--- src/activity.jsx
+++ src/activity.jsx
@@ -29,8 +29,8 @@ const ExtraActivity = ({ dark, setDark }) => {
         </h2>
         <p className="mb-3">
           Worked as a{" "}
-          <span className="font-semibold">Reactjs Front-End Developer</span>
-          for <span className="font-semibold">1 years</span>, gaining hands-on
+          <span className="font-semibold">Reactjs Front-End Intern</span>
+          for <span className="font-semibold">6 Months</span>, gaining hands-on
           industrial-level knowledge of how real-world projects are built and
           managed.
         </p>
